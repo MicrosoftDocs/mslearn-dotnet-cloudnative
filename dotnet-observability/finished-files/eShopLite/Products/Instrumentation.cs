7bcc8f835005fb2e76b46c8f07910c4e9b9f9750
Phil Stollery <phils@contentmaster.com>
2023-11-13 08:47:06 +0000

    Adding observablility

---
+++ dotnet-observability/finished-files/eShopLite/Products/Instrumentation.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+using OpenTelemetry.Trace;
+
+namespace Products.Instrumentation;
+
+public class Instrumentation
+{
+    public static readonly string ActivitySourceName = "eShopLite.Products";
+
+    public ActivitySource ActivitySource { get; } = new ActivitySource(ActivitySourceName);
+}
+
+public static class InstrumentationExtensions
+{
+    public static TracerProviderBuilder AddWorkerInstrumentation(this TracerProviderBuilder tracerProviderBuilder)
+    {
+        return tracerProviderBuilder.AddSource(Instrumentation.ActivitySourceName);
+    }
+}
