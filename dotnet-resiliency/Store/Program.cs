829adf5fb89dc53032f10acc30e54f533307a32b
Phil Stollery <phils@contentmaster.com>
2024-02-22 09:55:48 +0000

    fixes for resiliency

--- dotnet-resiliency/Store/Program.cs
+++ dotnet-resiliency/Store/Program.cs
@@ -1,5 +1,6 @@
 using Store.Components;
 using Store.Services;
+using Microsoft.Extensions.Http.Resilience;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,9 @@ builder.Services.AddHttpClient<ProductService>(c =>
     var url = builder.Configuration["ProductEndpoint"] ?? throw new InvalidOperationException("ProductEndpoint is not set");
 
     c.BaseAddress = new(url);
+}).AddStandardResilienceHandler(options =>
+{
+    options.Retry.MaxRetryAttempts = 7;
 });
 
 // Add services to the container.
